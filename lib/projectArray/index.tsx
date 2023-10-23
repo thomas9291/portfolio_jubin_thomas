31ca3c96963a8ad00c083f406801ca52bb02aa9a
thomas <130037065+thomas9291@users.noreply.github.com>
2023-10-23 16:18:25 +0200

    detail page run

--- lib/projectArray/index.tsx
+++ lib/projectArray/index.tsx
@@ -4,6 +4,17 @@ export const projectArray: Project[] = [
   {
     projectId: "p1",
     title: "Fitness App",
+    programs: [
+      "html",
+      "css",
+      "bootstrap",
+      "mongodb",
+      "nextjs",
+      "reactjs",
+      "visualStudio",
+      "github",
+      "vercel",
+    ],
     date: "sep 2023",
     description:
       "This is a full stack project. Developed with: Reactjs, Nextjs, css, Bootstrap, Mongodb and mongoose.This is a fitness website for all people who want to train effectively with a weight adaptation plan...",
@@ -15,6 +26,17 @@ export const projectArray: Project[] = [
   {
     projectId: "p2",
     title: "Fishing Place App",
+    programs: [
+      "html",
+      "reactjs",
+      "nextjs",
+      "css",
+      "bootstrap",
+      "mongodb",
+      "visualStudio",
+      "github",
+      "vercel",
+    ],
     date: "jul 2023",
     description:
       "This is a full stack project, this project was my final project of the Spiced academy bootcamp berlin. Developed with: Reactjs, Nextjs, css, Bootstrap, Mongodb and mongoose. It's an application that's a sort of clone of Google Map for fishermen who want to remember their fishing spot...",
@@ -26,6 +48,7 @@ export const projectArray: Project[] = [
   {
     projectId: "p3",
     title: "Frontend App",
+    programs: ["javascript", "css", "html", "visualStudio", "github", "vercel"],
     date: "Apr 2022",
     description:
       "This is a frontend project. Developed with: Vanilla javascript, css and html. in this project, I work with an API which gives me photos of new and old films. By typing the name of a film or an actor, you can find several films in reference to your search.",
@@ -37,6 +60,7 @@ export const projectArray: Project[] = [
   {
     projectId: "p4",
     title: "Weather App",
+    programs: ["reactjs", "css", "visualStudio", "github", "vercel", "html"],
     date: "May 2023",
     description:
       "This is a React frontend project. Developed with Reactjs, css  and the local storage. In this project, I work with an API, which gives me random data on the whether. Depending on whether, we can make a list of things to do.",
--- src/pages/projects/[projectId].tsx
+++ src/pages/projects/[projectId].tsx
@@ -3,11 +3,77 @@ import { useRouter } from "next/router";
 import { projectArray } from "../../../lib/projectArray";
 import { ProjectCart } from "@/components/ProjectCart";
 
+import {
+  Aws,
+  Bootstrap,
+  Css3,
+  ExpressJs,
+  Github,
+  Html5,
+  Mongodb,
+  Nextjs,
+  ReactSvg,
+  Tailwind,
+  Typescript,
+  VisualStudio,
+  Vercel,
+  VanillaJs,
+} from "../../components/icons/svgItems/SvgItems";
+
 const ProjectDetail = () => {
   const router = useRouter();
   const { projectId } = router.query;
   const projectObj = projectArray.find((obj) => obj.projectId === projectId);
   console.log(projectObj);
+
+  function getIcons() {
+    let arr: React.JSX.Element[] = [];
+    if (projectObj?.programs.includes("javascript")) {
+      arr.push(<VanillaJs />);
+    }
+    if (projectObj?.programs.includes("css")) {
+      arr.push(<Css3 />);
+    }
+    if (projectObj?.programs.includes("html")) {
+      arr.push(<Html5 />);
+    }
+    if (projectObj?.programs.includes("bootstrap")) {
+      arr.push(<Bootstrap />);
+    }
+    if (projectObj?.programs.includes("mongodb")) {
+      arr.push(<Mongodb />);
+    }
+    if (projectObj?.programs.includes("nextjs")) {
+      arr.push(<Nextjs />);
+    }
+    if (projectObj?.programs.includes("reactjs")) {
+      arr.push(<ReactSvg />);
+    }
+    if (projectObj?.programs.includes("visualStudio")) {
+      arr.push(<VisualStudio />);
+    }
+    if (projectObj?.programs.includes("github")) {
+      arr.push(<Github />);
+    }
+    if (projectObj?.programs.includes("vercel")) {
+      arr.push(<Vercel />);
+    }
+    if (projectObj?.programs.includes("aws")) {
+      arr.push(<Aws />);
+    }
+    if (projectObj?.programs.includes("expressjs")) {
+      arr.push(<ExpressJs />);
+    }
+    if (projectObj?.programs.includes("tailwind")) {
+      arr.push(<Tailwind />);
+    }
+    if (projectObj?.programs.includes("typescript")) {
+      arr.push(<Typescript />);
+    }
+    return arr;
+  }
+  const iconsArray = getIcons();
+  console.log("get icons frm detailPage: ", iconsArray);
 
   return (
     <div>
@@ -15,6 +81,11 @@ const ProjectDetail = () => {
         title={projectObj?.title}
         date={projectObj?.date}
         description={projectObj?.description}
+        programs={iconsArray.map((element, index) => (
+          <ul key={index}>
+            <li>{element}</li>
+          </ul>
+        ))}
         videoURL={projectObj?.videoURL}
         linkURL={projectObj?.linkURL}
         codeURL={projectObj?.codeURL}
