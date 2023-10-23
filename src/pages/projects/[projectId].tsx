5e1aca50deefc3cd94dfc30e29e29b479aff38e1
thomas <130037065+thomas9291@users.noreply.github.com>
2023-10-23 14:35:50 +0200

    detail page run

--- src/pages/projects/[projectId].tsx
+++ src/pages/projects/[projectId].tsx
@@ -1,12 +1,24 @@
 import React from "react";
 import { useRouter } from "next/router";
+import { projectArray } from "../../../lib/projectArray";
+import { ProjectCart } from "@/components/ProjectCart";
 
 const ProjectDetail = () => {
   const router = useRouter();
-  const { projectId, title } = router.query;
+  const { projectId } = router.query;
+  const projectObj = projectArray.find((obj) => obj.projectId === projectId);
+  console.log(projectObj);
+
   return (
     <div>
-      <p>{projectId}</p>
+      <ProjectCart
+        title={projectObj?.title}
+        date={projectObj?.date}
+        description={projectObj?.description}
+        videoURL={projectObj?.videoURL}
+        linkURL={projectObj?.linkURL}
+        codeURL={projectObj?.codeURL}
+      />
     </div>
   );
 };
