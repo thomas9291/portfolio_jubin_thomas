bbda7b7ca280fadaf70818cb1563ccc60722b4c1
thomas <130037065+thomas9291@users.noreply.github.com>
2023-10-25 18:57:07 +0200

    useEfect added

--- src/components/slideVideo/index.tsx
+++ src/components/slideVideo/index.tsx
@@ -1,9 +1,10 @@
-import React, { useRef, useState } from "react";
+import React, { useEffect, useState } from "react";
 // Import Swiper React components
 import { Swiper, SwiperSlide } from "swiper/react";
 import CartVideo from "../cartVideo/index";
 import Link from "next/link";
 import { projectArray } from "../../../lib/projectArray";
+import { ErrorComponent } from "../errorComponent";
 
 // Import Swiper styles
 import "swiper/css";
@@ -12,57 +13,70 @@ import "swiper/css/pagination";
 
 import classes from "./slideVideo.module.css";
 
-// import required modules
 import { EffectCoverflow, Pagination } from "swiper/modules";
 import { Github } from "../icons/svgItems/SvgItems";
+import { Project } from "../../../project.model";
 
 export default function SlideVideo() {
-  return (
-    <div className={classes.container}>
-      <div className={classes.app}>
-        <Swiper
-          className={classes.swiper}
-          effect={"coverflow"}
-          grabCursor={true}
-          centeredSlides={true}
-          slidesPerView={"auto"}
-          coverflowEffect={{
-            rotate: 50,
-            stretch: 0,
-            depth: 100,
-            modifier: 1,
-            slideShadows: true,
-          }}
-          pagination={true}
-          modules={[EffectCoverflow, Pagination]}
-          /* className="mySwiper" */
-        >
-          {projectArray.map((element) => {
-            return (
-              <SwiperSlide
-                key={element.projectId}
-                className={classes.swiperSlide}
-              >
-                <div className={classes.img}>
-                  <CartVideo
-                    title={element.title}
-                    videoURL={element.videoURL}
-                    projectId={element.projectId}
-                  />
-                </div>
-              </SwiperSlide>
-            );
-          })}
-        </Swiper>
-        <div className={classes.info}>
-          <p>To see more projects, I invite you to visit my github:</p>
-          <p className={classes.link}>
-            <Link href={"https://github.com/thomas9291"} target="_blank">
-              <Github />
-            </Link>
-          </p>
+  const [projects, setProject] = useState([] as any);
+  const [isArray, setIsArray] = useState(false);
+  useEffect(() => {
+    setProject(projectArray);
+    setIsArray(true);
+  }, []);
+
+  if (isArray) {
+    return (
+      <div className={classes.container}>
+        <div className={classes.app}>
+          <Swiper
+            className={classes.swiper}
+            effect={"coverflow"}
+            grabCursor={true}
+            centeredSlides={true}
+            slidesPerView={"auto"}
+            coverflowEffect={{
+              rotate: 50,
+              stretch: 0,
+              depth: 100,
+              modifier: 1,
+              slideShadows: true,
+            }}
+            pagination={true}
+            modules={[EffectCoverflow, Pagination]}
+          >
+            {projects.map((element: Project) => {
+              return (
+                <SwiperSlide
+                  key={element.projectId}
+                  className={classes.swiperSlide}
+                >
+                  <div className={classes.img}>
+                    <CartVideo
+                      title={element.title}
+                      videoURL={element.videoURL}
+                      projectId={element.projectId}
+                    />
+                  </div>
+                </SwiperSlide>
+              );
+            })}
+          </Swiper>
+          <div className={classes.info}>
+            <p>To see more projects, I invite you to visit my github:</p>
+            <p className={classes.link}>
+              <Link href={"https://github.com/thomas9291"} target="_blank">
+                <Github />
+              </Link>
+            </p>
+          </div>
         </div>
       </div>
-    </div>
+    );
+  }
+  return (
+    <>
+      <ErrorComponent />
+    </>
   );
 }
--- src/pages/projects/index.tsx
+++ src/pages/projects/index.tsx
@@ -3,11 +3,12 @@ import SlideVideo from "../../components/slideVideo";
 
 export default function projects() {
   return (
-    <div>
+    <>
       <article className="flex flex-col justify-center items-center m-0 text-[#f5f3f1] bg-[#271b1b] ">
         <h1 className="text-xl">Some of my latest projects</h1>
       </article>
+
       <SlideVideo />
-    </div>
+    </>
   );
 }
