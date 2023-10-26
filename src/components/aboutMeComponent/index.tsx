a8fbc501532371ce864764c7bc7c8d71242a53f4
thomas <130037065+thomas9291@users.noreply.github.com>
2023-10-26 18:09:24 +0200

    “designfixed“

--- src/components/aboutMeComponent/index.tsx
+++ src/components/aboutMeComponent/index.tsx
@@ -8,21 +8,23 @@ export const AboutMeComponent = () => {
       <h2 className={classes.header2}>I am passionate about programming!</h2>
       <div className={classes.text}>
         <p>
-          I started learning by myself on the internet since the beginning of
-          2022.
-        </p>
-        <p>I had the chance to go to spiced academy from april to July 2023,</p>
-        <p>it is an intensive bootcamp based in berlin, </p>
-        <p>
-          it allowed me to learn with a group and to have the support of the
-          teachers.
+          I began teaching myself through online resources at the start of 2022.
         </p>
         <p>
-          Since then I have been able to practice and take online courses in
-          order to strengthen my knowledge.
+          I had the opportunity to attend the Spiced Academy from April to July
+          2023.
         </p>
-        <p>I am French and I have lived in Berlin since 2016.</p>
-        <p>I speak French, Spanish, German and English.</p>
+        <p>It is an intensive bootcamp located in Berlin. </p>
+        <p>
+          This experience enabled me to learn within a group setting and receive
+          support from the instructors.
+        </p>
+        <p>
+          Since then, I have been actively practicing and taking online courses
+          to further solidify my knowledge.
+        </p>
+        <p>I am French and have been residing in Berlin since 2016.</p>
+        <p>I am proficient in French, Spanish, German, and English.</p>
       </div>
     </div>
   );
