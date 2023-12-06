3c1f02823059764b0693f7362723c49d2dce9673
thomas <130037065+thomas9291@users.noreply.github.com>
2023-12-06 11:49:30 +0100

    contact page fixed

--- src/pages/contact/index.tsx
+++ src/pages/contact/index.tsx
@@ -3,7 +3,7 @@ import { ContactCarts } from "@/components/contactCarts";
 
 export default function contact() {
   return (
-    <div>
+    <div className="h-screen">
       <ContactCarts />
     </div>
   );
