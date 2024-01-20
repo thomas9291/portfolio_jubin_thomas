0e08597d9d207bd772c33c5573008c42eb1ceb3f
thomas <130037065+thomas9291@users.noreply.github.com>
2024-01-20 07:47:42 +0100

    icons effect added

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -18,6 +18,7 @@ export default function homePage() {
         <div>
           <Computer />
         </div>
+
         <div>
           <Telephon />
         </div>
--- src/pages/projects/index.tsx
+++ src/pages/projects/index.tsx
@@ -5,7 +5,7 @@ export default function projects() {
   return (
     <>
       <article className="flex flex-col justify-center items-center m-0 text-[#f5f3f1] bg-[#271b1b] ">
-        <h1 className="text-xl">Some of my latest projects</h1>
+        <h1 className="text-xl text-[#72e846]">Some of my latest projects</h1>
       </article>
       <div>
         <SlideVideo />
