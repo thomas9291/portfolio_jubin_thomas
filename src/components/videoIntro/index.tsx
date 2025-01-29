d75bf37a7086119e6b02a1002c1efaf2693968d7
thomas <130037065+thomas9291@users.noreply.github.com>
2025-01-29 14:37:05 +0100

    video intro added

---
+++ src/components/videoIntro/index.tsx
@@ -0,0 +1,21 @@
+import React, { useState, useEffect } from "react";
+
+import classes from "./videoIntro.module.css";
+import { PlayCircleIcon } from "@heroicons/react/20/solid";
+
+const VideoIntro: React.FC = () => {
+  return (
+    <div className={classes.presentationContainer}>
+      <video className={classes.screen} controls>
+        <source src={"/videos/nexusTraining6.mp4"} type="video/mp4" />
+      </video>
+      <div className={classes.containerText}>
+        <h1 className={classes.headers2}>Hi, MY NAME IS THOMAS JUBIN</h1>
+        <h1 className={classes.headers}>I AM 3D ARTIST </h1>
+        <h1 className={classes.headers}>AND FULL-STACK WEB DEVELOPER</h1>
+      </div>
+    </div>
+  );
+};
+
+export default VideoIntro;
